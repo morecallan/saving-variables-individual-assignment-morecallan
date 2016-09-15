67418106d5c878a0893b7e994907d20b564bd6fa
Callan Morrison <callan.l.morrison@gmail.com>
2016-09-15 10:15:10 -0500

    variables model properties and constructor defined

---
+++ SavingVariables/Models/Variable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingVariables.Models
+{
+    public class Variable
+    {
+        //Properties: => Table columns.
+        public char Var { get; set; }
+        public int Val { get; set; }
+
+        //Constructor: Can only be instantiated with dependencies.
+        public Variable(char _var, int _val)
+        {
+            Var = _var;
+            Val = _val;
+        }
+
+    }
+}
