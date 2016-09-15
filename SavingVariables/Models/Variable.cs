1a5cd0c82dabb7516e9010fac9109943c89836f1
Callan Morrison <callan.l.morrison@gmail.com>
2016-09-15 10:21:56 -0500

    updated variables model

--- SavingVariables/Models/Variable.cs
+++ SavingVariables/Models/Variable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace SavingVariables.Models
 {
     public class Variable
     {
+        [Key]
+        public int VariableId { get; set; }
+
         //Properties: => Table columns.
+        [Required]
         public char Var { get; set; }
+        [Required]
         public int Val { get; set; }
 
         //Constructor: Can only be instantiated with dependencies.
