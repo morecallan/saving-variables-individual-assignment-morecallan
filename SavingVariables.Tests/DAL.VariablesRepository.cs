6c174ae13b3063dd5c93fdd04cd8bf1b6f3f9360
Callan Morrison <callan.l.morrison@gmail.com>
2016-09-16 18:14:44 -0500

    creation of moq datastore

--- SavingVariables.Tests/DAL.VariablesRepository.cs
+++ SavingVariables.Tests/DAL.VariablesRepository.cs
@@ -4,6 +4,7 @@ using Moq;
 using System.Data.Entity;
 using SavingVariables.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SavingVariables.Tests
 {
@@ -13,5 +14,23 @@ namespace SavingVariables.Tests
         Mock<VariablesContext> mock_context { get; set; }
         Mock<DbSet<Variable>> mock_variable_table { get; set; }
         List<Variable> variable_list { get; set; }
+
+
+        public void ConnectMocksToDatastore()
+        {
+            var queryable_list = variable_list.AsQueryable();
+
+            // Each time Linq tries to query our "table", redirect the query to point at our queirable list INSTEAD of the real database
+            mock_variable_table.As<IQueryable<Variable>>().Setup(m => m.Provider).Returns(queryable_list.Provider);
+            mock_variable_table.As<IQueryable<Variable>>().Setup(m => m.Expression).Returns(queryable_list.Expression);
+            mock_variable_table.As<IQueryable<Variable>>().Setup(m => m.ElementType).Returns(queryable_list.ElementType);
+            mock_variable_table.As<IQueryable<Variable>>().Setup(m => m.GetEnumerator()).Returns(() => queryable_list.GetEnumerator());
+
+            // This is now our table the is representative of the REAL DATABASE.
+            mock_context.Setup(c => c.Variables).Returns(mock_variable_table.Object);
+
+            // Add stuff to our representatve table
+            mock_variable_table.Setup(t => t.Add(It.IsAny<Variable>())).Callback((Variable v) => variable_list.Add(v));
+        }
     }
 }
