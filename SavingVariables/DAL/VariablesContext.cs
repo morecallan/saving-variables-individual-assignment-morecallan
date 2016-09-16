53744ca171bbbee8fa9c2822a2abb1eee66e53c3
Callan Morrison <callan.l.morrison@gmail.com>
2016-09-15 22:27:00 -0500

    removed unnecessary usings

--- SavingVariables/DAL/VariablesContext.cs
+++ SavingVariables/DAL/VariablesContext.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Contexts;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 
 namespace SavingVariables.DAL
 {
-    public class VariablesContext : Context
+    public class VariablesContext : DbContext
     {
+        public virtual DbSet<Variable> Variables { get; set; }
     }
 }
+
+
