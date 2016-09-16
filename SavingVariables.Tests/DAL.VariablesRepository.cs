953bb4c32d2b81bd3817aa030eb5173f994de1a3
Callan Morrison <callan.l.morrison@gmail.com>
2016-09-16 17:57:55 -0500

    set up mocked variables and fake

--- SavingVariables.Tests/DAL.VariablesRepository.cs
+++ SavingVariables.Tests/DAL.VariablesRepository.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SavingVariables.DAL;
+using Moq;
 
 namespace SavingVariables.Tests
 {
     [TestClass]
     public class VariablesRepositoryTests
     {
+        Mock<VariablesContext> mock_context { get; set; }
+        Mock<DbSet<Variable>> mock_author_table { get; set; }
+        List<Variable> author_list { get; set; }
     }
 }
