28ddc094b8b53a13422b06f1a1f8443d7723d929
Callan Morrison <callan.l.morrison@gmail.com>
2016-09-19 21:33:07 -0500

    evaluation of variable commands

---
+++ SavingVariables/Evaluate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SavingVariables.DAL;
+using System.Text.RegularExpressions;
+
+namespace SavingVariables
+{
+    public class Evaluate
+    {
+        VariablesRepository database = new VariablesRepository(new VariablesContext());
+
+        private bool CheckToSeeIfCommandIsInvolved(string input)
+        {
+            string pattern = @"^(?<command>clear|remove|delete|show)\s*(?<target>all|[a-zA-Z])$";
+            Match match = Regex.Match(input, pattern);
+            if (match.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool CheckToSeeIfVariableIsBeingSet(string input)
+        {
+            string pattern = @"^(?<varSym>[a-zA-Z]{1})(\s*[\=]\s*)?(?<val>[1-9])?$";
+            Match match = Regex.Match(input, pattern);
+            if (match.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
